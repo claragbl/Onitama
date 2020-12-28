859cf98064161a0324f155c1c9d611fb821c6bc1
claragaubil <claragaubil@macbook-pro-de-clara.home>
2020-12-28 10:31:08 +0100

    création du dépot Onitama

---
+++ src/onitama_berchiche_gaubil/Partie.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onitama_berchiche_gaubil;
+
+/**
+ *
+ * @author claragaubil
+ */
+public class Partie {
+    
+    //on créé les attributs:
+    
+    
+    
+}
